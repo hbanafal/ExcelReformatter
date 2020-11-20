2c7de0d528f3c844be1b1adedfef51d537448124
Hemant <hemant.banafal@onefootball.com>
2020-11-20 19:32:49 +0100

    Added minimum value functionality

--- excel_reformatter.py
+++ excel_reformatter.py
@@ -5,25 +5,42 @@ import glob
 
 def combine_excel():
     combined_data = pd.DataFrame()
+    new_data = pd.DataFrame()
     files = glob.glob(get_prop("excel_path") + "/*.csv")
     if len(files) == 0:
         print("There are not input files on the path given in property file.")
         exit(1)
     for f in glob.glob(get_prop("excel_path") + "/*.csv"):
+        if 'BIG' in f:
+            print("New stocks file is present")
+            new_data = pd.read_csv(f, na_filter=False, skip_blank_lines=True)
+            continue
+
         df = pd.read_csv(f, na_filter=False, skip_blank_lines=True)
         combined_data = combined_data.append(df, ignore_index=True)
     print("All Files are read successfully")
 
-    if len(combined_data.index) == 1:
-        print("There is no data in the input CSV files.")
-        exit(1)
     combined_data = combined_data.drop_duplicates(subset=['Portfolio', 'TradingSymbol'])
     print("Duplicate Stocks are removed!!")
+
+    if len(new_data.index) > 1:
+        new_data = check_new_stocks(combined_data, new_data)
+        print("Total new stocks are ", len(new_data.index)/4)
+        new_data.to_csv(path_or_buf=get_prop("excel_path") + "/../NewStocks.csv", index=False, header=True)
 
     combined_data = update_rates(combined_data)
     print("All files are combined and conversion & reversal rates updated")
 
     return combined_data
+
+
+def check_new_stocks(old_data, new_data):
+    for index, row in new_data.iterrows():
+        if row['Portfolio'] in old_data['Portfolio'].values:
+            new_data.drop(index=index, inplace=True)
+            continue
+
+    return new_data
 
 
 def update_rates(combined_data):
@@ -33,25 +50,35 @@ def update_rates(combined_data):
             continue
         stock_price = str(row['Portfolio']).split('_')[2]
         stock_name = str(row['Portfolio']).split('_')[0]
-        rates = read_rates()[0]
-        generic_rate = rates['Rate']
-        if stock_name in rates:
-            generic_rate = rates[stock_name]
+        rates_df = read_rates()
+        generic_rate = rates_df.at["Rate", "Value"]
+        min_value = rates_df.at["Rate", "Min"]
+        if stock_name in rates_df.index:
+            generic_rate = rates_df.at[stock_name, "Value"]
+            if rates_df.at[stock_name, "Min"] not in {"", "nan", "NaN", "null", "NULL"}:
+                min_value = rates_df.at[stock_name, "Min"]
 
-        if generic_rate != 'NC':
-            modified_value = float(stock_price) * float(generic_rate)
-            modified_value = .05 * round(modified_value / .05)
-            modified_value = round(modified_value, 2)
-            combined_data.at[index, 'conversionrate'] = modified_value
-            combined_data.at[index, 'reversalrate'] = modified_value
+        if generic_rate == 'NC':
+            continue
+        elif generic_rate == 'DEL':
+            combined_data.drop(index=index, inplace=True)
+            continue
+
+        modified_value = float(stock_price) * float(generic_rate)
+        modified_value = .05 * round(modified_value / .05)
+        modified_value = round(modified_value, 2)
+        if modified_value < min_value:
+            modified_value = min_value
+
+        combined_data.at[index, 'conversionrate'] = modified_value
+        combined_data.at[index, 'reversalrate'] = modified_value
 
     return combined_data
 
 
 def read_rates():
-    df = pd.read_csv(get_prop("excel_path") + "/../RateInput.csv", names=['Rates', 'Value'], delimiter=';', header=0)
-    input_rates = df.set_index('Rates').T.to_dict('records')
-    return input_rates
+    df = pd.read_csv(get_prop("excel_path") + "/../RateInput.csv", index_col=0, delimiter=';', header=0)
+    return df
 
 
 def create_output_csv(no_of_files):
@@ -69,4 +96,4 @@ def create_output_csv(no_of_files):
             i = 1
 
 
-create_output_csv(10)
+create_output_csv(1)
--- stt_nstt_update.py
+++ stt_nstt_update.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from read_prop import get_prop
 import glob
-import re
 from datetime import datetime
+
 
 def update_stt_data():
     stt_data = pd.DataFrame()
@@ -31,61 +31,98 @@ def update_stt_data():
 
 
 def update_rates(data):
-    current_month = str(datetime.now().strftime('%B')).upper()[:3]
+    current_month = get_month(datetime.now().month)
+    next_month = get_month(datetime.now().month + 1)
+    rates_df = read_stt_rates()
+
+    max_order_lot = rates_df.at["MAX_ORDER_LOT", "Cutting>10"]
+    generic_cutting_rate_gt_10 = rates_df.at["Rate", "Cutting>10"]
+    generic_min_cutting_rate_gt_10 = rates_df.at["Rate", "MinCut>10"]
+    generic_cutting_rate_lt_10 = rates_df.at["Rate", "Cutting<10"]
+    generic_making_rate_lt_10 = rates_df.at["Rate", "Making<10"]
+    generic_min_cutting_rate_lt_10 = rates_df.at["Rate", "MinCut<10"]
+    generic_min_making_rate_lt_10 = rates_df.at["Rate", "MinMak<10"]
+
     for index, row in data.iterrows():
+
         if str(row['Portfolio']) in {"", "nan", "NaN", "null", "NULL"}:
             data.drop(index=index, inplace=True)
             continue
         stock_range = str(row['Portfolio']).split('_')[2]
         stock_range = float(stock_range.split("TO")[0])
-        stock_name = str(row['Portfolio']).split('_')[0].split(current_month)[0]
-        stock_price = float(str(row['Portfolio']).split('_')[0].split(current_month)[1])
-        print("Stock price is ", stock_price)
-        rates_df = read_stt_rates()
-
-        max_order_lot = rates_df.at["MAX_ORDER_LOT", "Cutting>10"]
+        stock_name = str(row['Portfolio']).split('_')[0]
+        stock_price = 0
+        if current_month in stock_name:
+            stock_price = float(stock_name.split(current_month)[1])
+            stock_name = stock_name.split(current_month)[0]
+        elif next_month in stock_name:
+            stock_price = float(stock_name.split(next_month)[1])
+            stock_name = stock_name.split(next_month)[0]
+        else:
+            print("There is no current month ", current_month, " or next month ", next_month, " in Portfolio name ",
+                  stock_name)
 
         data.at[index, 'maxorderlot'] = max_order_lot
 
+        # This if block handles the stocks with range greater than 10
         if stock_range >= 10:
+            cutting_rate_gt_10 = generic_cutting_rate_gt_10
+            min_cutting_rate_gt_10 = generic_min_cutting_rate_gt_10
 
+            # Updating cutting rate and minimum cutting rate if the stock is listed in Input file
             if stock_name in rates_df.index:
-                cutting_rate = rates_df.at[stock_name, "Cutting>10"]
-            else :
-                cutting_rate = rates_df.at["Rate", "Cutting>10"]
+                cutting_rate_gt_10 = rates_df.at[stock_name, "Cutting>10"]
+                if rates_df.at[stock_name, "MinCut>10"] not in {"", "nan", "NaN", "null", "NULL"}:
+                    min_cutting_rate_gt_10 = rates_df.at[stock_name, "MinCut>10"]
 
-            if cutting_rate == 'NC':
+            # If cutting rate is NC (No Change) then skipping that stock and not updating anything except maxorderlot.
+            if cutting_rate_gt_10 == 'NC':
                 continue
 
-            print("Stock name is ", stock_name, "Rate is ", cutting_rate)
+            print("Stock name is ", stock_name, "Rate is ", cutting_rate_gt_10)
             data.at[index, 'Otm'] = 'no'
+
+            # Updating conversion rate or reversal rate.
             if float(data.at[index, 'conversionrate']) > 0:
-                data.at[index, 'conversionrate'] = modified_rate(stock_price, cutting_rate)
+                data.at[index, 'conversionrate'] = modified_rate(stock_price, cutting_rate_gt_10, None, min_cutting_rate_gt_10)
             else:
-                data.at[index, 'reversalrate'] = modified_rate(stock_price, cutting_rate)
+                data.at[index, 'reversalrate'] = modified_rate(stock_price, cutting_rate_gt_10, None, min_cutting_rate_gt_10)
+
+        # This else block handles the stocks with range less than 10
         elif stock_range < 10:
+            cutting_rate_lt_10 = generic_cutting_rate_lt_10
+            making_rate_lt_10 = generic_making_rate_lt_10
+            min_cutting_rate_lt_10 = generic_min_cutting_rate_lt_10
+            min_making_rate_lt_10 = generic_min_making_rate_lt_10
 
+            # Updating cutting rate, making rate, minimum cutting rate and minimum making rate if the stock is listed
+            # in Input file
             if stock_name in rates_df.index:
-                cutting_rate = rates_df.at[stock_name, "Cutting<10"]
-                making_rate = rates_df.at[stock_name, "Making<10"]
-            else :
-                cutting_rate = rates_df.at["Rate", "Cutting<10"]
-                making_rate = rates_df.at["Rate", "Making<10"]
+                cutting_rate_lt_10 = rates_df.at[stock_name, "Cutting<10"]
+                making_rate_lt_10 = rates_df.at[stock_name, "Making<10"]
+                if rates_df.at[stock_name, "MinMak<10"] not in {"", "nan", "NaN", "null", "NULL"}:
+                    min_making_rate_lt_10 = rates_df.at[stock_name, "MinMak<10"]
+                if rates_df.at[stock_name, "MinCut<10"] not in {"", "nan", "NaN", "null", "NULL"}:
+                    min_cutting_rate_lt_10 = rates_df.at[stock_name, "MinCut<10"]
 
-            if cutting_rate == 'NC' and making_rate == 'NC':
+            # If cutting rate and making rate is NC (No Change) then skipping that stock and not updating anything
+            # except maxorderlot.
+            if cutting_rate_lt_10 == 'NC' and making_rate_lt_10 == 'NC':
                 continue
 
-            print("Stock name is ", stock_name, "Rates are ", cutting_rate, making_rate)
+            print("Stock name is ", stock_name, "Rates are ", cutting_rate_lt_10, making_rate_lt_10)
 
+            # Updating conversion rate or reversal rate.
             if float(data.at[index, 'conversionrate']) == 0:
-                data.at[index, 'conversionrate'] = modified_rate(stock_price, making_rate, data.at[index, 'conversionrate'])
-                data.at[index, 'reversalrate'] = modified_rate(stock_price, cutting_rate, data.at[index, 'reversalrate'])
+                data.at[index, 'conversionrate'] = modified_rate(stock_price, making_rate_lt_10, data.at[index, 'conversionrate'], min_making_rate_lt_10)
+                data.at[index, 'reversalrate'] = modified_rate(stock_price, cutting_rate_lt_10, data.at[index, 'reversalrate'], min_cutting_rate_lt_10)
             else :
-                data.at[index, 'conversionrate'] = modified_rate(stock_price, cutting_rate, data.at[index, 'conversionrate'])
-                data.at[index, 'reversalrate'] = modified_rate(stock_price, making_rate, data.at[index, 'reversalrate'])
+                data.at[index, 'conversionrate'] = modified_rate(stock_price, cutting_rate_lt_10, data.at[index, 'conversionrate'], min_cutting_rate_lt_10)
+                data.at[index, 'reversalrate'] = modified_rate(stock_price, making_rate_lt_10, data.at[index, 'reversalrate'], min_making_rate_lt_10)
 
             data.at[index, 'Otm'] = 'yes'
 
+            # Updating conversion lot or reversal lot.
             if int(data.at[index, 'conversionlot']) == 0 and int(data.at[index, 'conversionrate']) > 0:
                 data.at[index, 'conversionlot'] = 10
             elif int(data.at[index, 'reversallot']) == 0 and int(data.at[index, 'reversalrate']) > 0:
@@ -94,12 +131,14 @@ def update_rates(data):
     return data
 
 
-def modified_rate(stock_price, rate, current_rate):
+def modified_rate(stock_price, rate, current_rate, min_value):
     if rate == 'NC':
         return current_rate
     modified_value = stock_price * float(rate)
     modified_value = .05 * round(modified_value / .05)
     modified_value = round(modified_value, 2)
+    if modified_value < min_value:
+        modified_value = min_value
     return modified_value
 
 
@@ -108,5 +147,12 @@ def read_stt_rates():
     return df
 
 
-update_stt_data()
+def get_month(month):
+    months = {1: "JAN", 2: "FEB", 3: "MAR", 4: "APR", 5: "MAY", 6: "JUN", 7: "JUL", 8: "AUG", 9: "SEP", 10: "OCT",
+              11: "NOV", 12: "DEC"}
+    if month > 12:
+        return months.get(1)
+    else:
+        return months.get(month)
 
+update_stt_data()
